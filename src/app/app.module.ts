e0c1cc48c78e2d5548cd8e9e1645b5ddef9e81b0
Khouch Koeun <khouch.koeun@gmail.com>
2018-05-24 23:12:02 +0700

    Enable blugin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { Geolocation } from '@ionic-native/geolocation';
 import { MyApp } from './app.component';
 
 import { AboutPage } from '../pages/about/about';
@@ -10,6 +11,18 @@ import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+
+
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+export const firebaseConfig = {
+  apiKey: "AIzaSyAi5ZZs0r69g_-4yuIlA24v2VKNPbWf1CI",
+  authDomain: "ionic-map-geo.firebaseapp.com",
+  databaseURL: "https://ionic-map-geo.firebaseio.com",
+  projectId: "ionic-map-geo",
+  storageBucket: "ionic-map-geo.appspot.com",
+  messagingSenderId: "981889444044"
+};
 
 @NgModule({
   declarations: [
@@ -21,7 +34,11 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp), 
+
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFirestoreModule
+
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -34,6 +51,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   providers: [
     StatusBar,
     SplashScreen,
+    Geolocation,
+    AngularFirestoreModule,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,96 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, Platform } from 'ionic-angular';
+import { Geolocation } from '@ionic-native/geolocation';
+import { AlertController } from 'ionic-angular';
+
+import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from 'angularfire2/firestore';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+
+interface User {
+  id: number;
+  name: string;
+  phone: string; 
+  lat:string;
+  lon:string; 
+}
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  userCol: AngularFirestoreCollection<User>;
+  users: Observable<User[]>;
+  public username:string = '';
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private platform: Platform, public afs:AngularFirestore, private geolocation: Geolocation, private alertCtrl: AlertController) {
 
+    platform.ready().then(() => {
+      
+      this.username = localStorage.getItem('username');
+      if(this.username != null){
+        this.watchGeo(this.username);
+      }else{
+        this.presentPrompt(); 
+      }
+
+     
+     
+
+     
+      //Getting the users
+      this.userCol = this.afs.collection('users');
+      this.users = this.userCol.valueChanges();
+      
+
+    });
+
+
+
+   
   }
+
+  presentPrompt() {
+    let alert = this.alertCtrl.create({
+      title: 'User',
+      inputs: [
+        {
+          name: 'username',
+          placeholder: 'Enter username here!'
+        }
+      ],
+      buttons: [
+    
+        {
+          text: 'Set',
+          handler: data => {
+           if(data.username != ''){
+            localStorage.setItem('username', data.username); 
+            this.watchGeo(data.username);
+           }
+          }
+        }
+      ]
+    });
+
+    alert.present();
+  }
+
+  watchGeo(username:string = ''){
+    const watch = this.geolocation.watchPosition().subscribe(pos => {
+      var data = {
+        lat: pos.coords.latitude, 
+        lon: pos.coords.longitude
+      }
+
+      this.afs.collection('users').doc(username).update(data); 
+
+  });
+  }
+
+
+
+
 
 }
